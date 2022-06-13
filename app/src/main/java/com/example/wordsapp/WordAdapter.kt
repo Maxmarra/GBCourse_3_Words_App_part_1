06e122d1427f341cb66dc56f2c031c209a371fd1
Maxmarra <5919669@gmail.com>
2022-06-13 12:35:33 +0300

    clean_starter_1

--- app/src/main/java/com/example/wordsapp/WordAdapter.kt
+++ app/src/main/java/com/example/wordsapp/WordAdapter.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 
+
 /**
  * Adapter for the [RecyclerView] in [DetailActivity].
  */

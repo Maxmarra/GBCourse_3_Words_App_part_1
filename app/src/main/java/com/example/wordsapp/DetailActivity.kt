446dcc6806816b691e4a1fd0e2e37a245e52444f
Maxmarra <5919669@gmail.com>
2022-06-13 13:41:04 +0300

    2

--- app/src/main/java/com/example/wordsapp/DetailActivity.kt
+++ app/src/main/java/com/example/wordsapp/DetailActivity.kt
@@ -6,10 +6,11 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wordsapp.databinding.ActivityDetailBinding
 
-
-
 class DetailActivity : AppCompatActivity() {
 
+//There's also a keyword companion, meaning it's associated
+// with the DetailActivity class,
+// and we don't need to give it a separate type name.
     companion object{
         const val LETTER = "letter"
     }
@@ -26,6 +27,9 @@ class DetailActivity : AppCompatActivity() {
         // Retrieve the LETTER from the Intent extras
         // intent.extras.getString returns String? (String or null)
         // so toString() guarantees that the value will be a String
+
+        // intent? is a property of any activity
+        // keeps a reference to the intent used to launch the activity
         val letterId = intent?.extras?.getString(LETTER).toString()
 
         val recyclerView = binding.recyclerView
